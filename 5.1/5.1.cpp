0e6fbd42476f50d5dbe3af7deb75cee84698e676
Artem <Artem@AAA.com>
2023-10-27 09:38:34 +0300

    Добавьте файлы проекта.

---
+++ 5.1/5.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int num;
+    int sum = 0;
+
+    cout <<"Введите число: ";
+    cin >> num;
+
+    while (num != 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    cout << "сумма = " << sum << endl;
+
+    return 0;
+}
