091fa2a5d613d2d860206d715e7335330fb536ab
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-27 10:01:42 +0300

    Add files via upload

---
+++ 5.2/5.2/5.2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0,"");
+    int num, i;
+    cout << "Введите число: ";
+    cin >> num;
+    for (i = 0; i < 100; i++) {
+        if (num == 0) break;
+        else num /= 10;
+    }
+    cout << i << "\n";
+}
