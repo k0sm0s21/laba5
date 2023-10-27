420d0f2f18984d46796aa7e7066d30f6509a9fca
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-27 10:22:32 +0300

    Add files via upload

---
+++ 5.3/5.3/5.3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int count = 0;
+    int arr[10] = { 4,47,75,89,42,94,2,75,15,44 };
+    for (int i = 0; i < 10; i++) {
+        count = count + arr[i];
+    }
+    cout << "Сумма всех элементов массива " << count << endl;
+    return 0;
+}
