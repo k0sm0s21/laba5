65d03e094cf63bd0653082e71d469152175bd514
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-27 11:00:44 +0300

    Add files via upload

---
+++ 5.4/5.4/5.4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int count = 0;
+    int arr[5] = { 25,37,12,558,118 };
+    for (int i = 0; i < 5; i++) {
+        count = count + arr[i];
+    }
+    cout << "Cреднее арифметическое элементов массива: " << count / 5 << endl;
+    return 0;
+}
